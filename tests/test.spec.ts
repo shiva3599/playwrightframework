7197420dd02f71bd7235b6ab55e1047b17da9bc0
Shivanand Miraje <shivamiraje456@gmail.com>
2026-05-30 19:29:07 +0530

    test file added

---
+++ tests/test.spec.ts
@@ -0,0 +1,9 @@
+import {test, expect} from '@playwright/test'
+
+test('title', async ({ page }) => {
+  
+   await page.goto('https://playwright.dev');
+   
+   await expect(page).toHaveTitle(/Playwright/);
+ 
+});
